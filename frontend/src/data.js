e40e44021f8077ef344d9028a1ba3f46c6f96b3e
ion-cirnici <48316733+ion-cirnici@users.noreply.github.com>
2021-04-05 23:51:40 +0100

    Switched to frontend React Fixes #3

---
+++ frontend/src/data.js
@@ -0,0 +1,68 @@
+export default { 
+    /*subObjects products*/
+    products:[
+        {
+        name: 'S10',
+        category: 'Samsung',
+        image: '/images/10Samsung1.jpg',
+        price: 450,
+        brand: 'Samsung',
+        rating: 4.6,
+        numReviews: 10
+    },
+    {
+        name: '12iPhonePro',
+        category: 'iPhone',
+        image: '/images/12iPhonePro1.jpg',
+        price: 799,
+        brand: 'iPhone',
+        rating: 4.7,
+        numReviews: 9
+    },
+    {
+        name: 'HuaweiMatePro40',
+        category: 'Huawei',
+        image: '/images/40HuaweiMatePro1.jpg',
+        price: 840,
+        brand: 'Huawei',
+        rating: 4.9,
+        numReviews: 12
+    },
+    {
+        name: 'S10',
+        category: 'Samsung',
+        image: '/images/10Samsung1.jpg',
+        price: 450,
+        brand: 'Samsung',
+        rating: 4.6,
+        numReviews: 10
+    },
+    {
+        name: 'S10',
+        category: 'Samsung',
+        image: '/images/10Samsung1.jpg',
+        price: 450,
+        brand: 'Samsung',
+        rating: 4.6,
+        numReviews: 10
+    },
+    {
+        name: 'S10',
+        category: 'Samsung',
+        image: '/images/10Samsung1.jpg',
+        price: 450,
+        brand: 'Samsung',
+        rating: 4.6,
+        numReviews: 10
+    },
+    {
+        name: 'S10',
+        category: 'Samsung',
+        image: '/images/10Samsung1.jpg',
+        price: 450,
+        brand: 'Samsung',
+        rating: 4.6,
+        numReviews: 10
+    },
+]
+}
